6097ec3bf3abc11c5dc0e7b86f8395352c93d6a7
coconutpalmtreeisland <65150534+SeoEugene@users.noreply.github.com>
2023-12-06 10:36:20 +0900

    commit

---
+++ src/assets/script/animation.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // main lodding
+    let title = document.querySelector(".title");
+    let circlBox = document.querySelector(".circle__box");
+    let mainBottom = document.querySelector(".main__bottom");
+    let dice = document.querySelector(".dice");
+    let starBox = document.querySelector(".star__box");
+    title.classList.add("loaded");
+    circlBox.classList.add("loaded");
+    mainBottom.classList.add("loaded");
+    dice.classList.add("loaded");
+    starBox.classList.add("loaded");
+
+    let backgroundChangeDark = document.querySelector(".background__change__dark");
+    let backgroundChangeYllow = document.querySelector(".background__change__yllow");
+
+    let body = document.querySelector("body");
+    backgroundChangeDark.addEventListener("click", () => {
+        body.classList.remove("yelow");
+        body.classList.add("sky");
+    });
+    backgroundChangeYllow.addEventListener("click", () => {
+        body.classList.remove("sky");
+        body.classList.add("yellow");
+    });
+    // contact lodding
+    // let contactLeft = document.querySelector(".contact__left");
+    // let contactRight = document.querySelector(".contact__right");
+    // contactLeft.classList.add("loaded");
+    // contactRight.classList.add("loaded");
+});
