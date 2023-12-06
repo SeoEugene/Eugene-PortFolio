7a450e8e1e8269329bd007b2557e6c925e5146c7
coconutpalmtreeisland <65150534+SeoEugene@users.noreply.github.com>
2023-12-06 10:50:17 +0900

    commit

--- src/assets/script/animation.js
+++ src/assets/script/animation.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     // main lodding
-    let title = document.querySelector(".title");
-    let circlBox = document.querySelector(".circle__box");
-    let mainBottom = document.querySelector(".main__bottom");
-    let dice = document.querySelector(".dice");
-    let starBox = document.querySelector(".star__box");
-    title.classList.add("loaded");
-    circlBox.classList.add("loaded");
-    mainBottom.classList.add("loaded");
-    dice.classList.add("loaded");
-    starBox.classList.add("loaded");
+    // let title = document.querySelector(".title");
+    // let circlBox = document.querySelector(".circle__box");
+    // let mainBottom = document.querySelector(".main__bottom");
+    // let dice = document.querySelector(".dice");
+    // let starBox = document.querySelector(".star__box");
+    // title.classList.add("loaded");
+    // circlBox.classList.add("loaded");
+    // mainBottom.classList.add("loaded");
+    // dice.classList.add("loaded");
+    // starBox.classList.add("loaded");
 
     let backgroundChangeDark = document.querySelector(".background__change__dark");
     let backgroundChangeYllow = document.querySelector(".background__change__yllow");
@@ -23,9 +23,5 @@ document.addEventListener("DOMContentLoaded", function () {
         body.classList.remove("sky");
         body.classList.add("yellow");
     });
-    // contact lodding
-    // let contactLeft = document.querySelector(".contact__left");
-    // let contactRight = document.querySelector(".contact__right");
-    // contactLeft.classList.add("loaded");
-    // contactRight.classList.add("loaded");
+
 });
